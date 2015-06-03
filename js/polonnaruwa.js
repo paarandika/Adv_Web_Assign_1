d32197b1b91f506dfe657e33f2736446d96a8933
paarandika <paarandika@gmail.com>
2015-06-04 01:50:02 +0530

    Added responsive menu

--- js/polonnaruwa.js
+++ js/polonnaruwa.js
@@ -1,3 +1,27 @@
+$(function() {
+    var pull        = $('#pull');
+        menu        = $('nav ul');
+        menuHeight  = menu.height();
+ 
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $('nav ul li').on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+});
 
 
-
+$(window).resize(function(){
+	$(".right").css({'float':'none'})
+    var w = $(window).width();
+    if(w > 510 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    }
+    if(w > 510) {
+    	$(".right").css({'float':'right'})
+    }
+}); 
